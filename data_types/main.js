c25ffc5c3b6bd6d9cbe2b5a066a5e108f040f8bb
Justin Gong <Justin@Justins-MacBook-Air.local>
2018-10-06 13:54:59 -0500

    commit for data types

---
+++ data_types/main.js
@@ -0,0 +1,25 @@
+const tempC = 3;
+
+console.log(tempC);
+
+// const room = 'conference';
+// const currentRoom = 'room1';
+// const inf = Infinity;
+// const bigInt = Number.MAX_SAFE_INTEGER;
+// const bigNum = Number.MAX_VALUE;
+// const message = `The maximum value is ${bigNum}`;
+// const obj = {
+//   size: 8,
+//   color: 'yellow',
+// };
+// const arr = [1, 2, 3, 4, 5];
+// const now = new Date();
+// const numStr = '4.56';
+// const num = Number(numStr);
+
+// const a = parseInt('15 monkeys', 10);
+
+const n = 33.5;
+const s = n.toString();
+
+console.log(s);
